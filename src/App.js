ee428e1879a235b8f82db2c50fbe656fe24d25fb
Huy Nguyen <huy@gohuygo.com>
2017-08-16 21:26:54 -0700

    comments

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
     super(props)
 
     var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+    // TODO: This should be a form input
     this.address = '0x3c5167c93dbaa26f945fba324dc0f7e07c9f18a8'
     this.accountBalance = web3.fromWei(web3.eth.getBalance(this.address), 'ether');
   }
@@ -24,15 +26,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>EtherSearch</h2>
         </div>
-        <div className="App-Content">
-          <table>
-            <thead>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
-
-        </div>
+        <p> Search Bar Here? </p>
       </div>
     );
   }
