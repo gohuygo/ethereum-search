45f7052312b301a70a7c00d8ccc5925cb9790ccf
Huy Nguyen <huy@gohuygo.com>
2017-08-16 20:58:34 -0700

    getting console log to work

--- src/App.js
+++ src/App.js
@@ -4,46 +4,30 @@ import './App.css';
 import Web3 from 'web3';
 import _ from 'lodash';
 
-var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+// var ETHEREUM_CLIENT = new Web3()
 
 //TODO: peopleContractABI and peopleContractAddress should be input values\
 
 //People.deployed().then(a=>JSON.stringify(a.abi))
-var peopleContractABI = [{"constant":true,"inputs":[],"name":"getPeople","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_firstName","type":"bytes32"},{"name":"_lastName","type":"bytes32"},{"name":"_age","type":"uint256"}],"name":"addPerson","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"people","outputs":[{"name":"firstName","type":"bytes32"},{"name":"lastName","type":"bytes32"},{"name":"age","type":"uint256"}],"payable":false,"type":"function"}]
+// var peopleContractABI = [{"constant":true,"inputs":[],"name":"getPeople","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_firstName","type":"bytes32"},{"name":"_lastName","type":"bytes32"},{"name":"_age","type":"uint256"}],"name":"addPerson","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"people","outputs":[{"name":"firstName","type":"bytes32"},{"name":"lastName","type":"bytes32"},{"name":"age","type":"uint256"}],"payable":false,"type":"function"}]
 
 //People.deployed().then(a=>a.address)
-var peopleContractAddress = '0x57fb626bf9ed3c54203b971ec1b74ba8313f3998'
-
-var peopleContract = ETHEREUM_CLIENT.eth.contract(peopleContractABI).at(peopleContractAddress)
+// var bittrex = '0xfbb1b73c4f0bda4f67dca266ce6ef42f520fbb98'
+// var bittrexAccounts = ETHEREUM_CLIENT.fromWei(ETHEREUM_CLIENT.eth.getBalance(bittrex));
 
 class App extends Component {
   constructor(props) {
+    console.warning("TEST?!")
+
     super(props)
-    this.state = {
-      firstNames: [],
-      lastNames: [],
-      ages: []
-    }
   }
   componentWillMount(){
-    var data = peopleContract.getPeople()
-    this.setState({
-      firstNames: String(data[0]).split(','),
-      lastNames: String(data[1]).split(','),
-      ages: String(data[2]).split(',')
-    })
+    // console.log(bittrexAccounts)
   }
   render() {
-    var TableRows = []
-    _.each(this.state.firstNames, (value, index) =>{
-      TableRows.push(
-        <tr>
-          <td>{ETHEREUM_CLIENT.toAscii(this.state.firstNames[index])}</td>
-          <td>{ETHEREUM_CLIENT.toAscii(this.state.lastNames[index])}</td>
-          <td>{this.state.ages[index]}</td>
-        </tr>
-      )
-    })
+    console.log("WTF")
+    // console.log(bittrex)
+    // console.log(bittrexAccounts)
 
     return (
       <div className="App">
@@ -61,10 +45,8 @@ class App extends Component {
               </tr>
             </thead>
             <tbody>
-              {TableRows}
             </tbody>
           </table>
-
 
         </div>
       </div>
